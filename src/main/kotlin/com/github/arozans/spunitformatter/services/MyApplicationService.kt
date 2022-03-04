1de0a972b01dd5de770ec4d0b0a1ac9ef674a85a
GitHub Action <action@github.com>
2022-03-04 09:55:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/arozans/spunitformatter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/arozans/spunitformatter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.arozans.spunitformatter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arozans.spunitformatter.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/arozans/spunitformatter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arozans/spunitformatter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arozans.spunitformatter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arozans.spunitformatter.MyBundle
 
 class MyProjectService(project: Project) {
 
